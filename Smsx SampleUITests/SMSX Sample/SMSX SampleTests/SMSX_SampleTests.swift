8781d03e381e41b9af15da80fc698d6837471983
farhan182 <43361302+farhan182@users.noreply.github.com>
2018-09-18 10:36:49 +0800

    Add files via upload

---
+++ Smsx SampleUITests/SMSX Sample/SMSX SampleTests/SMSX_SampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  SMSX_SampleTests.swift
+//  SMSX SampleTests
+//
+//  Created by High Siera on 08/03/2018.
+//  Copyright © 2018 High Siera. All rights reserved.
+//
+
+import XCTest
+@testable import SMSX_Sample
+
+class SMSX_SampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
